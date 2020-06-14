e1cf35dcd9556ac11ae15bcf563799f289bddddd
Michał Szala <michalmszala@gmail.com>
2020-06-15 00:45:24 +0200

    Added budget summary panel.

--- frontend/src/app/budget/budget-summary-panel/budget-summary-panel.component.ts
+++ frontend/src/app/budget/budget-summary-panel/budget-summary-panel.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { BudgetModel } from 'src/app/api/models';
 
 @Component({
   selector: 'app-budget-summary-panel',
   templateUrl: './budget-summary-panel.component.html',
-  styleUrls: ['./budget-summary-panel.component.scss']
+  styleUrls: ['./budget-summary-panel.component.scss'],
 })
 export class BudgetSummaryPanelComponent implements OnInit {
+  @Input() public budget: BudgetModel;
 
-  constructor() { }
+  public allIncomes: number;
+  public allExpenses: number;
+  public balance: number;
+
+  public estimated: number;
+  public signed: number;
+  public prepaid: number;
+  public paid: number;
+
+  constructor() {}
 
   ngOnInit() {
+    this.allIncomes = this.sum(this.budget.incomes);
+    this.allExpenses = this.sum(this.budget.expenses);
+    this.balance = this.allIncomes - this.allExpenses;
+
+    this.estimated = this.sumByExpenseType(1);
+    this.signed = this.sumByExpenseType(2);
+    this.prepaid = this.sumByExpenseType(3);
+    this.paid = this.sumByExpenseType(4);
   }
 
+  private sum(amounts: any[]): number {
+    return amounts.map((t) => t.amount).reduce((acc, value) => acc + value, 0);
+  }
+
+  private sumByExpenseType(expenseType: number): number {
+    return this.sum(this.budget.expenses.filter((e) => e.expenseStatus === expenseType));
+  }
 }
